626edccc0ea96043a9bf8f10a79043b7d3905741
Vyacheslav Shebanov <vyacheslavshebanov@Terminal.local>
2021-01-29 03:13:08 +0300

    addd init config

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE DeriveGeneric  #-}
+{-# LANGUAGE DeriveAnyClass #-}
 
 module Lib
     ( startApp
@@ -8,13 +10,24 @@ module Lib
 import App (Env(..), AppEnv, ServerEnv(..), AuthorEnv(..), BlogEnv(..))
 import Server (app)
 import Network.Wai.Handler.Warp (run)
+import Data.Yaml (decodeFileThrow, FromJSON)
+import GHC.Generics (Generic)
 
+
+data InitConfig = InitConfig {
+  serverDomain :: String,
+  serverProtocol :: String
+} deriving (Generic, FromJSON)
+
+getConfig :: IO InitConfig
+getConfig = decodeFileThrow "./config.yaml"
 
 mkAppEnv :: IO AppEnv
 mkAppEnv = do
+    initConfig <- getConfig
     let notesFolder = "./notes"
     let ttl = 3600
-    let senv = ServerEnv "localhost:8080/" "http:"
+    let senv = ServerEnv (serverDomain initConfig) (serverProtocol initConfig)
     let authorEnv = AuthorEnv "Viacheslav Shebanov" "terminal2010@gmail.com"
     let blogEnv = BlogEnv "Software Decay"
     pure Env{..}
