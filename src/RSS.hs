b0f213c254a5ca25b0ac2aaa19d110889487b347
Vyacheslav Shebanov <vyacheslavshebanov@terminal.lan>
2021-02-06 23:47:19 +0300

    atoms -> rss

--- src/RSS.hs
+++ src/RSS.hs
@@ -12,18 +12,16 @@ module RSS
 import Data.Typeable
 import Servant.API.ContentTypes
 import qualified Network.HTTP.Media as M
-import Text.Atom.Feed.Export (textFeed)
 import Data.Text.Encoding (encodeUtf8)
 import Data.ByteString.Lazy (fromStrict)
-import Data.Maybe (fromMaybe)
-import Data.Text.Lazy (empty, toStrict)
-import Text.Atom.Feed (Feed)
+import Data.Text (pack)
 import qualified Data.List.NonEmpty as NE
+import qualified Text.RSS as R
 
 data RSS deriving Typeable
 
 class ToRSS a where
-  toRss :: a -> Feed
+  toRss :: a -> R.RSS
 
 instance Accept RSS where
   contentTypes _ =
@@ -31,4 +29,4 @@ instance Accept RSS where
       [ "application" M.// "atom+xml" ]
 
 instance (ToRSS a) => MimeRender RSS a where
-    mimeRender _ = fromStrict . encodeUtf8 . toStrict . (fromMaybe empty) . textFeed . toRss
+    mimeRender _ = fromStrict . encodeUtf8 . pack . R.showXML . R.rssToXML . toRss
